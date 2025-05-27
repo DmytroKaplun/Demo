f7f1e4b5503910861ef2d7f5f75a027cfccc998c
DmytroKaplun <newtonknight1990@gmail.com>
2025-05-27 19:37:04 +0300

    fix

--- task03/jsrc/main/java/com/task03/HelloWorld.java
+++ task03/jsrc/main/java/com/task03/HelloWorld.java
@@ -4,24 +4,39 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.DeploymentRuntime;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
 @LambdaHandler(
-    lambdaName = "hello_world",
-	roleName = "hello_world-role",
-	isPublishVersion = true,
-	aliasName = "${lambdas_alias_name}",
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+		lambdaName = "hello_world",
+		roleName = "hello_world-role",
+		runtime= DeploymentRuntime.JAVA11,
+		isPublishVersion = true,
+		aliasName = "${lambdas_alias_name}",
+		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 
 	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
+		context.getLogger().log("Invocation started: " + getTimeStamp());
+
+		String message = "Hello from Lambda";
+
+		Map<String, Object> resultMap = new HashMap<>();
 		resultMap.put("statusCode", 200);
-		resultMap.put("message", "Hello from Lambda");
+		resultMap.put("message", message);
+
+		context.getLogger().log(message);
+
+		context.getLogger().log("Invocation completed: " + getTimeStamp());
 		return resultMap;
+	}
+
+	private String getTimeStamp() {
+		return new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(Calendar.getInstance().getTime());
 	}
 }
