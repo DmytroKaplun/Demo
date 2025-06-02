79ccb42fdab66853fb6c9ac46d290af3f5bb4b84
DmytroKaplun <newtonknight1990@gmail.com>
2025-06-02 12:05:01 +0300

    fix

--- task09/jsrc/main/java/com/task09/ApiHandler.java
+++ task09/jsrc/main/java/com/task09/ApiHandler.java
@@ -131,15 +131,15 @@ public class ApiHandler implements RequestHandler<Map<String, Object>, Map<Strin
 			orderedWeatherData.put("message", String.format("Error: %s", e.getMessage()));
 		}
 
-//		Gson gson = new Gson();
-//		String jsonResponse = gson.toJson(orderedWeatherData);
+		Gson gson = new Gson();
+		String jsonResponse = gson.toJson(orderedWeatherData);
 //		response.put("statusCode", 200); // HTTP status code
-		response.put("response", orderedWeatherData);
+		response.put("response", jsonResponse);
 
-		Map<String, Object> finalResponse = new LinkedHashMap<>();
-		finalResponse.put("statusCode", 200);
-		finalResponse.put("headers", headers);
-		finalResponse.put("body", response);
+//		Map<String, Object> finalResponse = new LinkedHashMap<>();
+//		finalResponse.put("statusCode", 200);
+//		finalResponse.put("headers", headers);
+//		finalResponse.put("body", response);
 		return response; // Return the LinkedHashMap response
 	}
 
